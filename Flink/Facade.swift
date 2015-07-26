d33c90a46e5c279250b11f906b722bce1812917f
Renan <renan.061@gmail.com>
2015-07-26 14:35:51 -0300

    Inicial

---
+++ Flink/Facade.swift
@@ -0,0 +1,15 @@
+//
+//  Facade.swift
+//  Flink
+//
+//  Created by Renan Almeida on 7/26/15.
+//  Copyright (c) 2015 Flink. All rights reserved.
+//
+
+import Foundation
+
+// Singleton
+class Facade
+{
+    
+}
